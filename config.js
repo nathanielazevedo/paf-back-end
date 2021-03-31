e81310fa1de9d369ad5996651be63566eff77982
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-03-30 18:01:16 -0700

    config

--- config.js
+++ config.js
@@ -3,16 +3,17 @@ const SECRET_KEY = "azevedo_hidden";
 
 require("dotenv").config();
 
-function getDatabaseUri() {
-  return process.env.NODE_ENV === "test"
-    ? "paf_test"
-    : process.env.DATABASE_URL || "paf";
-}
+
+  function getDatabaseUri() {
+    return process.env.NODE_ENV === "test"
+      ? "postgresql:///paf_test"
+      : process.env.DATABASE_URL || "postgresql:///paf";
+  }
   
 const BCRYPT_WORK_FACTOR = 12;
 
 module.exports = {
-    DB_URI,
+    // DB_URI,
     SECRET_KEY,
   BCRYPT_WORK_FACTOR,
     getDatabaseUri
--- db.js
+++ db.js
@@ -1,13 +1,25 @@
 const { Client } = require('pg');
+
+
 const { getDatabaseUri } = require("./config");
 
+let DB_URI = getDatabaseUri();
 
-const client = new Client({
-  connectionString: getDatabaseUri(),
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+let ssl;
+if (DB_URI !== "postgresql:///paf") {
+  ssl = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }
+} else {
+  ssl = null;
+}
+
+  const client = new Client({
+    connectionString: DB_URI,
+    ssl
+  });
 
 client.connect();
 
