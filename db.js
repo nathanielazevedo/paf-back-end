cadd041600722d0eff6905397c622f96b159f690
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-04-09 20:32:24 -0700

    db

--- db.js
+++ db.js
@@ -5,22 +5,22 @@ const { getDatabaseUri } = require("./config");
 
 let DB_URI = getDatabaseUri();
 
-// let ssl;
-// if (DB_URI !== "postgresql:///paf") {
-//   ssl = {
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   }
-// } else {
-//   ssl = null;
-// }
-//  console.log(ssl)
+let ssl;
+if (DB_URI !== "postgresql:///paf") {
+  ssl = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }
+} else {
+  ssl = null;
+}
+
   const client = new Client({
     connectionString: DB_URI,
-    // ssl: {
-    //   rejectUnauthorized: false,
-    // },
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
 client.connect();
